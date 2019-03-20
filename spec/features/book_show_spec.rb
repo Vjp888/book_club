343dbdb1958c32c475fea4ef542a5e2fd8519c15
Matt Levy <milevy1@gmail.com>
2019-03-19 21:57:30 -0600

    Added spec for Book Show page to show reviews

--- spec/features/book_show_spec.rb
+++ spec/features/book_show_spec.rb
@@ -21,4 +21,35 @@ RSpec.describe 'Book Show Page', type: :feature do
       expect(page).to have_content("Author 2 name")
     end
   end
+
+  it 'shows a list of reviews for that book' do
+    book_1 = Book.create(thumbnail: 'steve.jpg',
+                         title: 'Where The Wild Things Are',
+                         pages: 40,
+                         year_published: 1987)
+    review_1 = book_1.reviews.create(username: 'User1',
+                                     rating: 5,
+                                     title: 'Great book',
+                                     description: 'This book was great!')
+    review_2 = book_1.reviews.create(username: 'User2',
+                                     rating: 2,
+                                     title: 'Not great',
+                                     description: 'This book was bad.')
+
+    visit "/books/#{book_1.id}"
+
+    within "#review-#{review_1.id}" do
+      expect(page).to have_content("User1")
+      expect(page).to have_content("5/5")
+      expect(page).to have_content("Great book")
+      expect(page).to have_content("This book was great!")
+    end
+
+    within "#review-#{review_2.id}" do
+      expect(page).to have_content("User2")
+      expect(page).to have_content("2/5")
+      expect(page).to have_content("Not great")
+      expect(page).to have_content("This book was bad.")
+    end
+  end
 end
