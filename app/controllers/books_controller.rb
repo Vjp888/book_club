404073fe7ecfd66c395f910aba3ecb1fcf100a1f
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-03-20 16:39:20 -0600

    Adds Create Books action all tests passing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,31 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    # binding.pry
+    if @book.save
+      params[:author_list].split(',').map do |author|
+        @book.authors << Author.find_or_create_by(name: author.titleize.strip)
+      end
+      redirect_to book_path(@book)
+    else
+      @author_list = params[:author_list]
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    bp = params.require(:book).permit(:title, :pages, :year_published, :thumbnail)
+    bp[:thumbnail] = 'https://www.libreture.com/static/images/book-placeholder.png' unless bp[:thumbnail] != ""
+    bp[:title].titleize
+    bp
+  end
 end
