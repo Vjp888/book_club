77f395d6813fdd729bb7347b67fb750664d21815
Matt Levy <milevy1@gmail.com>
2019-03-20 18:00:43 -0600

    Added start of seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Author.destroy_all
+BookAuthor.destroy_all
+Book.destroy_all
+Review.destroy_all
