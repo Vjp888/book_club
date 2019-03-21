2f71aa87e42ee006cd9d9310858cfc1e8be8036d
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-03-21 12:19:45 -0600

    Adds test for book show page statistics

--- spec/features/book_show_spec.rb
+++ spec/features/book_show_spec.rb
@@ -51,4 +51,64 @@ RSpec.describe 'Book Show Page', type: :feature do
       expect(page).to have_content("This book was bad.")
     end
   end
+
+  describe 'Book show page statistics' do
+    it 'shows top and bottom three reviews for a book' do
+      author = Author.create(name: 'bob')
+      book = author.books.create(thumbnail: 'steve.jpg', title: 'where the wild things are', pages: 40, year_published: 1987)
+      book.reviews.create(rating: 5, title: "meh", description: "whahhednd vijnvsihb", username: "bob")
+      book.reviews.create(rating: 4, title: "haha", description: "whahhednd vijnvsihb", username: "rob")
+      book.reviews.create(rating: 3, title: "whatever", description: "whahhednd vijnvsihb", username: "harbi")
+      book.reviews.create(rating: 2, title: "is horrible", description: "whahhednd vijnvsihb", username: "stub")
+      book.reviews.create(rating: 1, title: "super bad", description: "whahhednd vijnvsihb", username: "rude")
+
+      visit book_path(book)
+
+      within '.average-rating' do
+        expect(page).to have_content("Rating: 3")
+      end
+
+      within '.book-stats' do
+        within '.top-reviews' do
+          expect(page).to have_content("Title: meh")
+          expect(page).to have_content("Rating: 5")
+          expect(page).to have_content("User: bob")
+          expect(page).to have_link("bob")
+
+          expect(page).to have_content("Title: haha")
+          expect(page).to have_content("Rating: 4")
+          expect(page).to have_content("User: rob")
+          expect(page).to have_link("rob")
+
+          expect(page).to have_content("Title: whatever")
+          expect(page).to have_content("Rating: 3")
+          expect(page).to have_content("User: harbi")
+          expect(page).to have_link("harbi")
+
+          expect(page).to_not have_content("is horrible")
+          expect(page).to_not have_content("super bad")
+        end
+
+        within '.bottom-reviews' do
+          expect(page).to have_content("super bad")
+          expect(page).to have_content("Rating: 1")
+          expect(page).to have_content("User: rude")
+          expect(page).to have_link("rude")
+
+          expect(page).to have_content("is horrible")
+          expect(page).to have_content("Rating: 2")
+          expect(page).to have_content("User: stub")
+          expect(page).to have_link("stub")
+
+          expect(page).to have_content("whatever")
+          expect(page).to have_content("Rating: 3")
+          expect(page).to have_content("User: harbi")
+          expect(page).to have_link("harbi")
+
+          expect(page).to_not have_content("haha")
+          expect(page).to_not have_content("meh")
+        end
+      end
+    end
+  end
 end
