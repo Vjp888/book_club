feac17b36573ed7b36bc8929590a530a5412de34
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-03-21 14:14:13 -0600

    Adds requested edgecasing for fewer reviews and if there are no reviews present

--- spec/features/book_show_spec.rb
+++ spec/features/book_show_spec.rb
@@ -110,5 +110,52 @@ RSpec.describe 'Book Show Page', type: :feature do
         end
       end
     end
+    it 'Shows a message when no reviews are present' do
+      author = Author.create(name: 'bob')
+      book = author.books.create(thumbnail: 'steve.jpg', title: 'where the wild things are', pages: 40, year_published: 1987)
+
+      visit book_path(book)
+      
+      expect(page).to_not have_content("Top Reviews")
+      expect(page).to_not have_content("Bottom Reviews")
+      expect(page).to have_link("Be the first to review this book")
+    end
+
+    it 'is able to show less than three reviews' do
+      author = Author.create(name: 'bob')
+      book = author.books.create(thumbnail: 'steve.jpg', title: 'where the wild things are', pages: 40, year_published: 1987)
+      book.reviews.create(rating: 5, title: "meh", description: "whahhednd vijnvsihb", username: "bob")
+      book.reviews.create(rating: 4, title: "haha", description: "whahhednd vijnvsihb", username: "rob")
+
+      visit book_path(book)
+
+      within '.book-stats' do
+        within '.top-reviews' do
+          expect(page).to have_content("Title: meh")
+          expect(page).to have_content("Rating: 5")
+          expect(page).to have_content("User: bob")
+          expect(page).to have_link("bob")
+
+          expect(page).to have_content("Title: haha")
+          expect(page).to have_content("Rating: 4")
+          expect(page).to have_content("User: rob")
+          expect(page).to have_link("rob")
+        end
+      end
+
+      within '.book-stats' do
+        within '.bottom-reviews' do
+          expect(page).to have_content("Title: haha")
+          expect(page).to have_content("Rating: 4")
+          expect(page).to have_content("User: rob")
+          expect(page).to have_link("rob")
+
+          expect(page).to have_content("Title: meh")
+          expect(page).to have_content("Rating: 5")
+          expect(page).to have_content("User: bob")
+          expect(page).to have_link("bob")
+        end
+      end
+    end
   end
 end
