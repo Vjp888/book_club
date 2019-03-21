f2777faafeb278ddda67aaac7c9250ed7f86b295
Matt Levy <milevy1@gmail.com>
2019-03-21 16:37:46 -0600

    Added route to new reviews nested books

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome_page#index'
 
-  resources :books, only: [:index, :show, :new, :create]
+  resources :books, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new]
+  end
   resources :authors, only: [:show]
 end
