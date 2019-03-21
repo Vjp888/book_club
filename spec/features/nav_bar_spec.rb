52b9dd11357806d52b30c4d75266354436a47fc1
Matt Levy <milevy1@gmail.com>
2019-03-21 13:15:47 -0600

    Added spec for clicking nav-bar links

--- spec/features/nav_bar_spec.rb
+++ spec/features/nav_bar_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe 'Navigation Bar', type: :feature do
         expect(page).to have_link('Browse Books', href: books_path)
       end
     end
+
+    it 'clicking the Browse Books link should take me to /books' do
+      visit root_path
+      click_link 'Browse Books'
+
+      expect(current_path).to eq(books_path)
+    end
   end
 
   context 'As a visitor to the books page' do
@@ -20,6 +27,13 @@ RSpec.describe 'Navigation Bar', type: :feature do
         expect(page).to have_link('Home', href: root_path)
         expect(page).to have_link('Browse Books', href: books_path)
       end
+    end
+
+    it 'clicking the Home link should take me to the root welcome page' do
+      visit root_path
+      click_link 'Home'
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
