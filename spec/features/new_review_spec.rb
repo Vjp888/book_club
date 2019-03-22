5d6585a0b93938c0e7860a932c183baa69733e96
Matt Levy <milevy1@gmail.com>
2019-03-22 17:43:48 -0600

    Added happy path spec for adding a new review to a book

--- spec/features/new_review_spec.rb
+++ spec/features/new_review_spec.rb
@@ -10,5 +10,51 @@ RSpec.describe 'Adding new review to book', type: :feature do
         expect(page).to have_link('Add review', href: new_book_review_path(book_1))
       end
     end
+
+    describe 'when I click on the link Add review' do
+      it 'takes me to a new review path' do
+        book_1 = Book.create(thumbnail: 'steve.jpg', title: 'where the wild things are', pages: 40, year_published: 1987)
+        visit book_path(book_1)
+        click_on 'Add review'
+
+        expect(current_path).to eq(new_book_review_path(book_1))
+      end
+    end
+
+    describe 'Happy path' do
+      describe 'when I fill in the form and click submit' do
+        before(:each) do
+          @book_1 = Book.create(thumbnail: 'steve.jpg', title: 'where the wild things are', pages: 40, year_published: 1987)
+          visit book_path(@book_1)
+          click_on 'Add review'
+
+          @username = 'User1'
+          @rating = 1
+          @title = 'Great book'
+          @description = 'I loved this book!'
+
+          fill_in :review_username, with: @username
+          fill_in :review_rating, with: @rating
+          fill_in :review_title, with: @title
+          fill_in :review_description, with: @description
+
+          click_on 'Create Review'
+        end
+
+        it 'returns to that books show page' do
+          expect(current_path).to eq(book_path(@book_1))
+        end
+
+        it 'shows new review on that books show page' do
+          review_1 = Review.last
+          within "#review-#{review_1.id}" do
+            expect(page).to have_content(@username)
+            expect(page).to have_content("#{@rating}/5")
+            expect(page).to have_content(@title)
+            expect(page).to have_content(@description)
+          end
+        end
+      end
+    end
   end
 end
