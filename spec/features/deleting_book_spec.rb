f2c592fe2ebf3123301d0a7bae324b936380cfc7
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-03-25 16:32:47 -0600

    Renames test to better cover what it is doing.
    Co-authored-by: Matt Levy <36040194+milevy1@users.noreply.github.com>

--- spec/features/deleting_book_spec.rb
+++ spec/features/deleting_book_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'when a user clicks the delete button', type: :feature do
-  describe 'happy path' do
-    it 'deletes a book from an author that has other books' do
-      author = Author.create(name: 'bob')
-      book_1 = author.books.create(thumbnail: 'steve.jpg', title: 'Book 1', pages: 40, year_published: 1987)
-      book_2 = author.books.create(thumbnail: 'steve.jpg', title: 'Book 2', pages: 40, year_published: 1987)
+  it 'deletes a book from an author that has other books and associated reviews' do
+    author = Author.create(name: 'bob')
+    book_1 = author.books.create(thumbnail: 'steve.jpg', title: 'Book 1', pages: 40, year_published: 1987)
+    book_2 = author.books.create(thumbnail: 'steve.jpg', title: 'Book 2', pages: 40, year_published: 1987)
+    book_1.reviews.create!(rating: 4, title: "haha", description: "whatever", username: "bob", created_at: Date.parse("dec 2 2017"))
+    book_1.reviews.create!(rating: 4, title: "haha", description: "whatever", username: "bob", created_at: Date.parse("dec 2 2017"))
 
-      visit book_path(book_1)
+    visit book_path(book_1)
 
-      expect(page).to have_link("Delete Book")
-      click_link 'Delete Book'
+    expect(page).to have_link("Delete Book")
+    expect(Review.count).to eq(2)
 
-      expect(current_path).to eq(books_path)
-      expect(page).to_not have_content(book_1.title)
-      expect(page).to have_content(book_2.title)
-    end
+    click_link 'Delete Book'
+
+    expect(current_path).to eq(books_path)
+    expect(page).to_not have_content(book_1.title)
+    expect(page).to have_content(book_2.title)
+    expect(page).to have_content("bob")
+    expect(Review.count).to eq(0)
   end
 end
