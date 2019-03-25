a19d4388c9fbdcef7706a16c91a0ce6c662ad1ae
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-03-25 16:35:14 -0600

    Implements delete function and dependent destroy to book model
    Co-authored-by: Matt Levy <36040194+milevy1@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+
+  end
+
   private
 
   def book_params
